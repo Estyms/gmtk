4ba712ce26f396f09cf2661f3841fe1bb8c43c88
Evann Regnault <evann.regnault@pm.me>
2022-07-17 20:31:13 +0200

    Tried a fix

--- Assets/Scripts/DiceFaces/BlackFace.cs
+++ Assets/Scripts/DiceFaces/BlackFace.cs
@@ -5,6 +5,14 @@ public class BlackFace : DiceFace
     public override void Action(Unit caster, Unit _, int value, GameState gameState)
     {
         caster.TakeDamage(value);
-        caster.InvokeAttackDone(false);
+        if (caster.enabled)
+        {
+            caster.InvokeAttackDone(false);
+        }
+        else
+        {
+            gameState.EndTurn();
+        }
+        
     }
 }
